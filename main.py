cf2b8f58e10c3d5d1648c6a5a7c0febb85149259
Kieran H <kieran.houtgraaf@gmail.com>
2025-01-08 18:05:26 +0100

    improvements

--- main.py
+++ main.py
@@ -9,42 +9,45 @@ words = w.read().split(',')
 w.close()
 #print(words)
 
-
-reader = PdfReader("text.pdf")
-
-parts = []
-
-def visitor_body(text, cm, tm, font_dict, font_size):
-	global parts
-	parts.append(text)
-
-def get_page(reader, page):
-	global parts
-	page = reader.pages[page]
-	
-	parts = []
-	
-	page.extract_text(visitor_text=visitor_body)
-	text_body = "".join(parts)
-
-	return "\n".join(text_body.split("\n")[:-1])
-
-#print(get_page(reader, 12))
-
 index = {}
 
-for i in range(len(reader.pages)):
-	had_words = []
-	for word in get_page(reader, i).split():
-		word = word.lower().translate(str.maketrans('','',"!\"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~”“’‘"))
-		if (word not in words) and (word not in had_words) and (len(word) > 1):
-			if word in index:
-				index[word].append(i)
-			else:
-				index[word] = [i]
-			had_words.append(word)
+
+page_no = 0#int(input("Starting page no"))
+had_words = []
+
+f = open("input.txt")
+text = f.read()
+f.close()
+
+def fix_first_break(text):
+	for i in range(len(text)-1):
+		if text[i] == "-" and text[i+1] == "\n":
+			return text[:i] + text[i+2:]
+	return text
+
+text_old = ""
+
+while text_old != text:
+	text_old = text
+	text = fix_first_break(text)
+
+text = text.replace("↵", "ff")
+
+for word in text.split():
+	if word == "PAGE":
+		had_words = []
+		page_no += 1
+
+	word = word.lower().translate(str.maketrans('','',"!\"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~”“’‘"))
+	if (word not in words) and (word not in had_words) and (len(word) > 1) and not word.isdigit():
+		if word in index:
+			index[word].append(page_no)
+		else:
+			index[word] = [page_no]
+		had_words.append(word)
 
 index = dict(sorted(index.items()))
+
 
 #print(index)
 
